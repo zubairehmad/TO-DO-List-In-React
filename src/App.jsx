4791577dc06aab3ac1eff1cf2b0ac66ac8e7d745
zubairehmad <zubairahmadcontact@gmail.com>
2024-12-23 14:46:01 +0500

    Add ToDoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import AppHeader from './AppHeader/AppHeader.jsx'
+import ToDoList from './ToDoList/ToDoList.jsx'
 
 function App() {
 	return (
-		<AppHeader />
+		<>
+			<AppHeader />
+			<ToDoList />
+		</>
 	)
 }
 
---
+++ src/ToDoList/ToDoList.jsx
@@ -0,0 +1,12 @@
+import './ToDoList.css'
+
+function ToDoList() {
+	return (
+		<ul>
+			<li>Complete your homework</li>
+			<li>Wake up early in the morning</li>
+		</ul>
+	)
+}
+
+export default ToDoList
