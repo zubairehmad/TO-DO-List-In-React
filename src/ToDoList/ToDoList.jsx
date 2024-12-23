8e3253681c08fa76b51ad7cef576e08d770f12c9
zubairehmad <zubairahmadcontact@gmail.com>
2024-12-23 17:12:53 +0500

    Add delete button and add funtionality to it

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,10 +1,13 @@
 import './ToDoList.css'
 
-function ToDoList({ toDoList }) {
+function ToDoList({ toDoList, deleteItem }) {
 	return (
 		<ul id="app-to-do-list">
 			{ toDoList.map((item, index) => (
-				<li key={index}>{item}</li>
+				<li key={index}>
+					{item}
+					<span className="trailing-close" onClick={() => deleteItem(index)}>×</span>
+				</li>
 			))}	
 		</ul>
 	)
